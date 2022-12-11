8e676d581bd04e56f9d02e22bf79d161aa50367d
Mark Whitehead <mjamesw2010@gmail.com>
2022-12-11 16:28:39 +0000

    Created a link from dashboard to prefrences

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ function App() {
       <h1>Application</h1>
       <BrowserRouter>
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/prefrences" element={<Prefrences />} />
         </Routes>
       </BrowserRouter>
--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Dashboard() {
     return(
+        <div>
         <h2>Dashboard</h2>
+        <Link to="/prefrences">Prefrences</Link>
+        </div>
     );
 }
 
