ccb02bdb9270e2a2b4c7aa17e5e376be39945f2d
Benjamin <benseed.bs@gmail.com>
2022-12-14 22:53:54 +0000

    implemented a marker on google map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,27 +1,31 @@
 import React from "react";
-import '../styles/Map.css';
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import "../styles/Map.css";
+import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
 
 export default function Home() {
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_EMBED_API_KEY
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_EMBED_API_KEY,
   });
 
-  console.log(process.env.REACT_APP_GOOGLE_MAPS_EMBED_API_KEY)
+  console.log(process.env.REACT_APP_GOOGLE_MAPS_EMBED_API_KEY);
 
   if (!isLoaded) return <div>Loading...</div>;
 
   return <Map />;
-  
 
   function Map() {
+    const center = {
+      lat: 53.483959,
+      lng: -2.244644,
+    };
     return (
       <GoogleMap
         zoom={12}
-        center={{ lat: 53.483959, lng: -2.244644 }}
+        center={center}
         mapContainerClassName="map-container"
       >
+        <MarkerF position={center} />
       </GoogleMap>
     );
   }
-};
+}
