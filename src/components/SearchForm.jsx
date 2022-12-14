52b570a5f614ab166780b4a8f487b90f7f0cb6b1
Mark Whitehead <mjamesw2010@gmail.com>
2022-12-14 21:50:06 +0000

    Added address to the googleLocation object

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,18 +1,19 @@
 import { useEffect } from "react";
 import axios from "axios";
 
-function SearchForm(props) {
+function SearchForm() {
 
   useEffect(() => {
     axios
       .get(
-        "/api/retrieve/registry/lat/53.483959/long/-2.244644/dist/2/limit/5/format/json"
+        "/api/retrieve/registry/lat/53.483959/long/-2.244644/dist/2/limit/30/format/json"
       )
       .then((response) => {
         const ChargingStation = response.data.ChargeDevice;
         const longitude = ChargingStation.map((e) => e.ChargeDeviceLocation.Longitude);
         const latitude= ChargingStation.map((e) => e.ChargeDeviceLocation.Latitude);
-        const googleLocation = latitude.map((element, index) => { return { lat: element, lng: longitude[index] } })
+        const address = ChargingStation.map((e) => e.ChargeDeviceLocation.Address)
+        const googleLocation = latitude.map((element, index) => { return { lat: element, lng: longitude[index], address: address[index] } })
         console.log(googleLocation);
         return(googleLocation);
       });
