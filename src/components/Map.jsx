38e236a71fd7fe3229490a78705a32f2842277b9
Benjamin <benseed.bs@gmail.com>
2022-12-15 23:23:18 +0000

    rendered multiple dynamic markers called from API onto map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import "../styles/Map.css";
 import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
 
 export default function Home() {
+  const [chargeSites, setchargeSites] = useState([]);
+
+  useEffect(() => {
+      fetch('/api/retrieve/registry/lat/53.483959/long/-2.244644/dist/50/limit/1000/format/json')
+      .then((res) => res.json())
+      .then((data) => {
+          setchargeSites(data.ChargeDevice)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+  }, [])
+
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_EMBED_API_KEY,
   });
 
   console.log(process.env.REACT_APP_GOOGLE_MAPS_EMBED_API_KEY);
+  console.log(chargeSites)
 
   if (!isLoaded) return <div>Loading...</div>;
 
@@ -18,13 +32,20 @@ export default function Home() {
       lat: 53.483959,
       lng: -2.244644,
     };
+
     return (
       <GoogleMap
         zoom={12}
         center={center}
         mapContainerClassName="map-container"
       >
-        <MarkerF position={center} />
+        {chargeSites.map(charger => { 
+          const { Latitude, Longitude } = charger.ChargeDeviceLocation
+          return <MarkerF
+          key={charger.ChargeDeviceId}
+          position={{lat: Number(Latitude), lng: Number(Longitude)}}
+          />
+        })}
       </GoogleMap>
     );
   }
