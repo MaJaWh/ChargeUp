9245436e31c00681d7a11c96d455d279ad72d73c
Mark Whitehead <mjamesw2010@gmail.com>
2022-12-20 22:40:44 +0000

    created InfoWindowF and rendered conditionally using position and chargedeviceId

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,20 @@
-import { React } from "react";
+import { React, useState } from "react";
 import "../styles/Map.css";
-import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, MarkerF, InfoWindowF } from "@react-google-maps/api";
 
 export default function Home({ userCoords, chargeSites }) {
-  console.log(userCoords);
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_EMBED_API_KEY,
   });
-
-  console.log(process.env.REACT_APP_GOOGLE_MAPS_EMBED_API_KEY);
 
   if (!isLoaded) return <div>Loading...</div>;
 
   return <Map />;
 
   function Map() {
+
+    const [position, setPosition] = useState(null)
+
     return (
       <GoogleMap
         zoom={12}
@@ -23,11 +23,16 @@ export default function Home({ userCoords, chargeSites }) {
       >
         {chargeSites.map((charger) => {
           const { Latitude, Longitude } = charger.ChargeDeviceLocation;
+          const { ChargeDeviceId } = charger;
           return (
             <MarkerF
+              onMouseOver={() => {setPosition(ChargeDeviceId)}}
+              onMouseOut={() => {setPosition(null)}}
               key={charger.ChargeDeviceId}
               position={{ lat: Number(Latitude), lng: Number(Longitude) }}
-            />
+            >
+              {(position === ChargeDeviceId)&&<InfoWindowF position={{ lat: Number(Latitude), lng: Number(Longitude) }}><div>Hello</div></InfoWindowF>}
+            </MarkerF>
           );
         })}
       </GoogleMap>
