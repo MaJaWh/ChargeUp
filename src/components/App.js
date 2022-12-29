34bb582c3e5752b8e41e89cc9e1b40ea7673ab31
Mark Whitehead <mjamesw2010@gmail.com>
2022-12-29 20:31:25 +0000

    removed protected route from client

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { React } from "react";
-import "../styles/App.css";
-import Dashboard from "../components/dashboard";
-import LandingPage from "./LandingPage";
-import SignUp from "./userAccounts/signUp.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { React } from 'react';
+import '../styles/App.css';
+import Dashboard from '../components/dashboard';
+import LandingPage from './LandingPage';
+import SignUp from './userAccounts/signUp.jsx';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/landingPage" element={<LandingPage />} />
           <Route path="/signUp" element={<SignUp />} />
-          <Route path="/" element={<Dashboard />} exact />
+          <Route path="/" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </div>
